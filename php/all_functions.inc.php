6358f59a68969045cfba3cc64ad4acf9ca9e4546
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-17 16:10:29 +0000

    Test methods

--- php/all_functions.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir = realpath(dirname(__FILE__)."/functions");
-foreach (glob($dir) as $filepath) {
-    if (is_file($filepath) && substr($filepath, -7) == ".inc.php") { 
-        include_once $filepath;
-    }
-}    
-?>
