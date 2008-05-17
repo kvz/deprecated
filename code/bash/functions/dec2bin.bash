8edf2b68b955e97d7a9fb07737e7e621783f1d65
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-17 18:38:55 +0000

    Added bash function

---
+++ code/bash/functions/dec2bin.bash
@@ -0,0 +1,23 @@
+#/**
+# * Convert a decimal to a binary string
+# * 
+# * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+# * @copyright 2007 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+# * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+# * @version   SVN: Release: $Id$
+# * @link      http://kevin.vanzonneveld.net/
+# * 
+# * @param integer INPUT the integer to convert
+# */
+function dec2bin { 
+    input=$1
+    result=""
+    while [ $input -gt 0 ]; do
+        if [ $((input%2)) -gt 0 ]; then
+            result="1$result"
+        else
+            result="0$result"
+        fi
+        input=$((input/2))
+    done;
+}
