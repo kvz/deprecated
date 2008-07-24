22ea9cba1f53193563e79e94c515685ffeda8b2e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-07-24 13:52:35 +0000

    Added takeOne function. Improved 2 other functions

--- code/php/functions/takeOne.inc.php
+++ code/php/functions/takeOne.inc.php
@@ -11,9 +11,16 @@
  */
 function takeOne($delimiter, &$string)
 {
+    // Explode into parts
     $parts  = explode($delimiter, $string);
+    
+    // Shift first
     $first  = array_shift($parts);
+    
+    // Glue back together, overwrite string by reference
     $string = implode($delimiter, $parts);
+    
+    // Return first part
     return $first;
 }
 ?>
