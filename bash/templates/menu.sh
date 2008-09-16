7c1c4431548fd38b58c78a6e5bea3ca474913a46
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-16 16:57:08 +0000

    Many improvements

--- bash/templates/menu.sh
+++ bash/templates/menu.sh
@@ -253,7 +253,7 @@ function kvzProgInstall() {
 # *
 # * @param string $1 KvzLib Program name
 # */
-function kvzProgInstall() {
+function kvzProgExecute() {
     # Check if dependencies are initialized
     if [ -z "${CMD_WGET}" ]; then
         echo "wget command not found or not initialized" >&2
