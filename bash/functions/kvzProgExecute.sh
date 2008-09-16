dd1c81f47a18a8fe18ebcc38cb46224e63585d6d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-16 16:43:22 +0000

    Fixed two minor bugs

---
+++ bash/functions/kvzProgExecute.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# * Tries to execute a bash program from remote KvzLib repository
+# * directly
+# *
+# * @param string $1 KvzLib Program name
+# */
+function kvzProgInstall() {
+    # Check if dependencies are initialized
+    if [ -z "${CMD_WGET}" ]; then
+        echo "wget command not found or not initialized" >&2
+        exit 1
+    fi
+
+    if [ -z "${CMD_PWD}" ]; then
+        echo "pwd command not found or not initialized" >&2
+        exit 1
+    fi
+
+    # Init
+    local PROGRAM=${1}
+    local KVZLIBURL="http://kvzlib.net/b"
+    local URL=${KVZLIBURL}/${PROGRAM}
+    
+    # Show
+    ${CMD_WGET} -qO- ${URL} |bash 
+    
+    if [ $? != 0 ]; then
+        echo "execution of ${URL} failed" >&2
+        exit 1
+    fi
+}
