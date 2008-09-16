8268f03ae6b462f6af5728b27a82dcdac14b2bd9
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-16 14:04:02 +0000

    Many improvements

--- bash/programs/setaptsources.sh
+++ bash/programs/setaptsources.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-
+#/**
+# * Resets Ubuntu APT sources.list
+# * And enables all the standard types: main restricted universe multiverse
+# * Makes a backup to /etc/apt/sources.list.bak
+# * 
+# * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+# * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+# * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+# * @version   SVN: Release: $Id$
+# * @link      http://kevin.vanzonneveld.net/
+# */
 MIRROR="nl"
 
 # Find lsb-release
