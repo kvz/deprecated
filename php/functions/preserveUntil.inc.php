d633e118d81f7d752bc669be947436f4704d1f39
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-07 11:35:57 +0000

    Added preserveUntil

---
+++ php/functions/preserveUntil.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Returns new array and preserves until a specific line is found in the old one.
+ * Ideal for rewriting config files with dynamic content, but still allowing
+ * custom rules above that. 
+ *
+ * @param array $original
+ * @param array $dynamic
+ * @param string $splitLine
+ * 
+ * @return array
+ */
+function preserveUntil($original=array(), $dynamic=array(), $splitLine = "# PLEASE DO NOT EDIT BELOW THIS LINE! #") {
+    $new = array();
+    
+    $splitLineAt = false;
+    foreach ($original as $n=>$line) {
+        if (trim($line) == trim($splitLine)) {
+            $splitLineAt = $n;
+            break;
+        }
+    }
+    
+    if (is_numeric($splitLineAt) && $splitLineAt) {
+        $new = array_slice($original, 0, ($splitLineAt-1));
+    }
+    
+    $new[] = $splitLine;
+    $new   = array_merge($new, $dynamic);
+    
+    return $new;
+}
+?>
