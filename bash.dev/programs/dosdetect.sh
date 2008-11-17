141942b44138c4fb869bb5e26e024dc1ab657ae5
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-11-17 14:11:51 +0000

    Added dosdetect

---
+++ bash.dev/programs/dosdetect.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#/**
+# * (NOT READY!) Detect if your machine is being DDoS-attacked
+# *
+# * Shows an ordered list of IP addresses that have connections 
+# * with your server 
+# * 
+# * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+# * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+# * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+# * @version   SVN: Release: $Id$
+# * @link      http://kevin.vanzonneveld.net/
+# */
+
+# Includes
+###############################################################
+source $(echo "$(dirname ${0})/../functions/log.sh")     # make::include
+source $(echo "$(dirname ${0})/../functions/toUpper.sh") # make::include
+source $(echo "$(dirname ${0})/../functions/commandInstall.sh") # make::include
+source $(echo "$(dirname ${0})/../functions/commandTest.sh") # make::include
+source $(echo "$(dirname ${0})/../functions/commandTestHandle.sh") # make::include
+source $(echo "$(dirname ${0})/../functions/getWorkingDir.sh") # make::include
+
+# Essential config
+###############################################################
+OUTPUT_DEBUG=0
+
+# Check for program requirements
+###############################################################
+commandTestHandle "bash" "bash" "EMERG" "NOINSTALL"
+commandTestHandle "aptitude" "aptitude" "DEBUG" "NOINSTALL" # Just try to set CMD_APTITUDE, produces DEBUG msg if not found
+commandTestHandle "sed" "sed" "DEBUG" "NOINSTALL" # Just try to set CMD_SED, helps with locating CMDs with dashes in it
+commandTestHandle "egrep" "grep" "EMERG"
+commandTestHandle "grep" "grep" "EMERG"
+commandTestHandle "awk" "gawk" "EMERG"
+commandTestHandle "head" "coreutils" "EMERG"
+commandTestHandle "sort" "coreutils" "EMERG"
+commandTestHandle "uniq" "coreutils" "EMERG"
+commandTestHandle "dirname" "coreutils" "EMERG"
+commandTestHandle "realpath" "realpath" "EMERG"
+commandTestHandle "sed" "sed" "EMERG"
+
+commandTestHandle "touch" "coreutils" "EMERG"
+commandTestHandle "sudo" "sudo" "EMERG" "NOINSTALL"
+commandTestHandle "netstat" "net-tools" "EMERG"
+
+# Config
+###############################################################
+
+
+# Run
+###############################################################
+${CMD_NETSTAT} -tepan |${CMD_AWK} '{print $5}' |${CMD_AWK} -F: '{print $1}' |${CMD_GREP} '\.' |${CMD_GREP} -v '0\.0\.0\.0' |${CMD_SORT} |${CMD_UNIQ} -c |${CMD_SORT} -rnk1 |${CMD_HEAD} -n30
