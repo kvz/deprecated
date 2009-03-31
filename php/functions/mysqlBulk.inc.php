ce7eb0a0f47cd188c9696b811a855e60fc6e2a8d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-03-31 14:33:00 +0000

    Fixing bugs in mysqlBulk

--- php/functions/mysqlBulk.inc.php
+++ php/functions/mysqlBulk.inc.php
@@ -3,13 +3,13 @@
  * Executes multiple queries in a 'bulk' to achieve better
  * performance and integrity.
  *
- * @param array  $queries
+ * @param array  $data
  * @param string $method
  * @param array  $options
  * 
  * @return float
  */
-function mysqlBulk(&$queries, $table, $method = 'concatenation', $options = array()) {
+function mysqlBulk(&$data, $table, $method = 'concatenation', $options = array()) {
     // Default options
     if (!isset($options['query_handler'])) {
         $options['query_handler'] = 'mysql_query';
@@ -25,20 +25,20 @@ function mysqlBulk(&$queries, $table, $method = 'concatenation', $options = arra
     }
 
     // Validation
-    if (!is_array($queries)) {
+    if (!is_array($data)) {
         if ($options['trigger_notices']) {
             trigger_error('First argument "queries" must be an array',
                 E_USER_NOTICE);
         }
         return false;
     }
-    if (count($queries) > 1000) {
+    if (count($data) > 1000) {
         if ($options['trigger_notices']) {
             trigger_error('It\'s recommended to use < 1000 queries/bulk',
                 E_USER_NOTICE);
         }
     }
-    if (empty($queries)) {
+    if (empty($data)) {
         return 0;
     }
 
@@ -50,13 +50,12 @@ function mysqlBulk(&$queries, $table, $method = 'concatenation', $options = arra
         }
     }
 
-    if (!function_exists('__execute')) {
-        function __execute($sql, $options) {
+    if (!function_exists('__exe')) {
+        function __exe($sql, $options) {
             extract($options);
-            
             if (!call_user_func($query_handler, $sql)) {
                 if ($trigger_errors) {
-                    trigger_error('Query failed.' .mysql_error(),
+                    trigger_error('Query failed.' .mysql_error() .'[sql: '.$sql.']',
                         E_USER_ERROR);
                     echo $sql."\n";
                     return false;
@@ -67,78 +66,131 @@ function mysqlBulk(&$queries, $table, $method = 'concatenation', $options = arra
         }
     }
 
+    if (!function_exists('__sql2array')) {
+        function __sql2array($sql, $options) {
+            extract($options);  
+            if (substr(strtoupper(trim($sql)), 0, 6) !== 'INSERT') {
+                if ($trigger_errors) {
+                    trigger_error('Magic sql2array conversion only works for inserts',
+                        E_USER_ERROR);
+                }
+                return false;
+            }
+
+            $parts   = preg_split("/[,\(\)] ?(?=([^'|^\\\']*['|\\\'][^'|^\\\']*['|\\\'])*[^'|^\\\']*[^'|^\\\']$)/", $sql);
+            $process = 'keys';
+            $data    = array();
+
+            foreach ($parts as $k=>$part) {
+                $tpart = strtoupper(trim($part));
+                if (substr($tpart, 0, 6) === 'INSERT') {
+                    continue;
+                } else if (substr($tpart, 0, 6) === 'VALUES') {
+                    $process = 'values';
+                    continue;
+                } else if (substr($tpart, 0, 1) === ';') {
+                    continue;
+                }
+                
+                if (!isset($data[$process])) $data[$process] = array();
+                $data[$process][] = $part;
+            }
+
+            return array_combine($data['keys'], $data['values']);
+        }
+    }
+
+
     // Make options local
     extract($options);
 
     // Start timer
     $start = microtime(true);
-    $count = count($queries);
+    $count = count($data);
 
     // Choose bulk method
     switch ($method) {
         case 'loaddata':
-            // Max 1200% gain
-            echo '-'.$method.'-'.$query_handler.'-'.count($queries).__LINE__."\n";
+            // Inserts data only
+            // Use array instead of queries
+            echo '-'.$method.'-'.$query_handler.'-'.count($data).__LINE__."\n";
             echo '    '.__c()." : ";
-            
-            if (!__execute(implode(';', $queries), $options)) {
-                return false;
+
+            $buf    = '';
+            foreach($data as $i=>$row) {
+                if (!is_array($row)) {
+                    $row = __sql2array($row, $options);
+                }
+                $buf .= implode(':::,', $row)."^^^\n";
             }
+            
+            $fields = implode(', ', array_keys($row));
+            
+            file_put_contents('/dev/shm/infile.txt', $buf);
+
+            if (!__exe("SET UNIQUE_CHECKS=0", $options)) return false;
+            if (!__exe("
+                LOAD DATA
+                CONCURRENT LOCAL INFILE '/dev/shm/infile.txt'
+                INTO TABLE ${table}
+                FIELDS TERMINATED BY ':::,'
+                LINES TERMINATED BY '^^^\\n'
+                (${fields})
+            ", $options)) return false;
 
             echo '    '.__c()."\n";
             break;
         case 'delayed':
             // MyISAM, MEMORY, ARCHIVE, and BLACKHOLE tables only!
-            echo '-'.$method.'-'.$query_handler.'-'.count($queries).__LINE__."\n";
+            echo '-'.$method.'-'.$query_handler.'-'.count($data).__LINE__."\n";
             echo '    '.__c()." : ";
 
-            if (!__execute(preg_replace('/$INSERT/', 'INSERT DELAYED',
-                        implode(';', $queries)), $options)) {
-                return false;
-            }
+            if (!__exe(preg_replace('/$INSERT/', 'INSERT DELAYED',
+                implode(';', $data)), $options)) return false;
             
             echo '    '.__c()."\n";
             break;
         case 'transaction':
+        case 'transaction_lock':
+        case 'transaction_nokeys':
             // Max 26% gain, but good for data integrity
-            echo '-'.$method.'-'.$query_handler.'-'.count($queries).__LINE__."\n";
+            echo '-'.$method.'-'.$query_handler.'-'.count($data).__LINE__."\n";
             echo '    '.__c()." : ";
             
-            if (!__execute('START TRANSACTION', $options)) {
-                return false;
+            if ($method == 'transaction_lock') {
+                if (!__exe('SET autocommit = 0', $options)) return false;
+                if (!__exe('LOCK TABLES '.$table.' READ', $options)) return false;
+            } else if ($method == 'transaction_keys') {
+                if (!__exe('ALTER TABLE '.$table.' DISABLE KEYS', $options)) return false;
             }
 
-            foreach ($queries as $query) {
-                if (!__execute($query, $options)) {
-                    __execute('ROLLBACK', $options);
+            if (!__exe('START TRANSACTION', $options)) return false;
+
+            foreach ($data as $query) {
+                if (!__exe($query, $options)) {
+                    __exe('ROLLBACK', $options);
+                    if ($method == 'transaction_lock') {
+                        __exe('UNLOCK TABLES '.$table.'', $options);
+                    }
                     return false;
                 }
             }
-            
-            if (!__execute('COMMIT', $options)) {
-                return false;
+
+            __exe('COMMIT', $options);
+
+            if ($method == 'transaction_lock') {
+                if (!__exe('UNLOCK TABLES', $options)) return false;
+            } else if ($method == 'transaction_keys') {
+                if (!__exe('ALTER TABLE '.$table.' ENABLE KEYS', $options)) return false;
             }
 
             echo '    '.__c()."\n";
             break;
-            echo '-'.$method.'-'.$query_handler.'-'.count($queries).__LINE__."\n";
-            echo '    '.__c()." : ";
-
-
-            if (!__execute('START TRANSACTION', $options)) {
-                return false;
-            }
-
-            if (!__execute(implode(';', $queries), $options)) {
-                __execute('ROLLBACK', $options);
-                return false;
-            }
-
-            if (!__execute('COMMIT', $options)) {
-                return false;
+        case 'none':
+            foreach ($data as $query) {
+                if (!__exe($query, $options)) return false;
             }
             
-            echo '    '.__c()."\n";
             break;
         case 'concatenation':
         case 'concat_trans':
@@ -164,7 +216,7 @@ function mysqlBulk(&$queries, $table, $method = 'concatenation', $options = arra
     $qps      = round ($count / $duration, 2);
 
     if ($eat_away) {
-        $queries = array();
+        $data = array();
     }
 
     // Return queries per second
