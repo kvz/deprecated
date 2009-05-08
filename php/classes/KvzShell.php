0269ae01cc2beaba7771f4eb738c58e47b3926f3
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-05-08 00:24:31 +0000

    A better isWritable

--- php/classes/KvzShell.php
+++ php/classes/KvzShell.php
@@ -724,6 +724,39 @@ class KvzShell {
         return false;
     }
 
+    /**
+     * A 'better' is_writable. Taken from PHP.NET comments:
+     * http://nl.php.net/manual/en/function.is-writable.php#73596
+     * Will work in despite of Windows ACLs bug
+     * NOTE: use a trailing slash for folders!!!
+     * see http://bugs.php.net/bug.php?id=27609
+     * see http://bugs.php.net/bug.php?id=30931
+     *
+     * @param string $path Path to test
+     *
+     * @return boolean
+     */
+    public static function isWritable($path)
+    {
+        if ($path{strlen($path)-1}=='/') {
+            //// recursively return a temporary file path
+            return self::isWritable($path.uniqid(mt_rand()).'.tmp');
+        } else if (is_dir($path)) {
+            return self::isWritable($path.'/'.uniqid(mt_rand()).'.tmp');
+        }
+        // check tmp file for read/write capabilities
+        $rm = file_exists($path);
+        $f  = @fopen($path, 'a');
+        if ($f === false) {
+            return false;
+        }
+        fclose($f);
+        if (!$rm) {
+            unlink($path);
+        }
+        return true;
+    }
+
     public function wget($url, $path, $dieOnFail = null) {
         if ($dieOnFail === null) {
             $dieOnFail = $this->getOption('die_on_fail');
