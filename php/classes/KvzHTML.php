c8663c4f1bf7bde4001f6a5e489947646f0352da
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-05-20 12:14:49 +0000

    KvzHTML

--- php/classes/KvzHTML.php
+++ php/classes/KvzHTML.php
@@ -12,48 +12,52 @@
  * @link      http://kevin.vanzonneveld.net/code/
  */
 Class KvzHtml {
-    public function __call($tag, $args) {
-        $body       = array_shift($args);
-        $this->tag($tag, $body, $args);
+    public function __call($tag, $arguments) {
+        $body       = array_shift($arguments);
+        $args       = array_shift($arguments);
+        return $this->tag($tag, $body, $args);
     }
-
-    public function tag($tag, $body, $args) {
-        if (is_array($arguments) && count($arguments)) {
-            $argumentsT = implode(' ', $arguments);
+    
+    public function tag($tag, $body = false, $args = array()) {
+        if (is_array($args) && count($args)) {
+            $argumentsT = implode(' ', $args);
         } else {
             $argumentsT = '';
         }
         
         if (false === $body) {
             // End tag
-            return '<'.$tag.' class="'.$class.'" '.$argumentsT.' />'."\n";
+            return '<'.$tag.' '.$argumentsT.' />'."\n";
         } else if (null === $body) {
             // Opening tag
-            return '<'.$tag.' class="'.$class.'" '.$argumentsT.'>'."\n";
+            return '<'.$tag.' '.$argumentsT.'>'."\n";
         } else {
             // Full tag
-            return '<'.$tag.' class="'.$class.'" '.$argumentsT.'>'."\n".$this->indent($body)."\n".'</'.$tag.'>'."\n";
+            return '<'.$tag.' '.$argumentsT.'>'."\n".$this->indent($body)."\n".'</'.$tag.'>'."\n";
         }
     }
-
+    
     public function hr() {
         return sprintf('<hr />'."\n");
     }
+    
     public function a($link, $title = '') {
         return sprintf('<a href="%s">%s</a>'."\n", $link, $title);
     }
+
     public function css($link) {
         return sprintf('<link rel="stylesheet" type="text/css" href="%s" />'."\n", $link);
     }
+
     public function js($link) {
         return sprintf('<script type="text/javascript" src="%s"></script>'."\n", $link);
     }
+
     public function img($link, $class = '') {
         return sprintf('<img src="%s" class="%s" />'."\n", $link, $class);
     }
-
+    
     public function indent($str) {
-
         $lines = explode("\n", $str);
         foreach ($lines as &$line) {
             $line = '    '. $line;
