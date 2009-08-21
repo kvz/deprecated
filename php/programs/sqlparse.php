0e023700ca8b94e662a304ab81a18b1ffb695314
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-08-21 11:08:45 +0000

    Sql parser

---
+++ php/programs/sqlparse.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if (!include_once('SQL/Parser.php')) {
+    echo "You need to: \n";
+    echo "    aptitude install php-pear\n";
+    echo "    pear install -f SQL_Parser\n";
+    exit(1);
+}
+
+
+$stdin = fopen('php://stdin', 'r');
+$sql   = '';
+while(!feof($stdin)) {
+    $sql .= fgets($stdin);
+}
+fclose($stdin);
+
+
+$sql        = str_replace('`', '', $sql);
+$SQL_Parser = new SQL_Parser($sql);
+$parsed     = $SQL_Parser->parse();
+echo join(', ', $parsed['table_names'])."\n";
+?>
