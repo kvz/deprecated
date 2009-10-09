eb147d194641ffe5a6a584a63316953a9e200c86
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-09 14:56:44 +0200

    Building improvements

--- samplemake.sh
+++ samplemake.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git status
+read -p "Clean working dir? Continue. Otherwise CTRL+C!" COMMENT
+
 set -x
 rootdir="$(realpath $(dirname $0))"
 phpdir="${rootdir}/php/samples"
@@ -6,7 +9,7 @@ for phpfile in `find ${phpdir} -type f -iname '*.php'`; do
   htmlfile=$(echo "${phpfile}" | sed 's#\.php#\.html#g')
   htmlfileinroot=$(echo "${htmlfile}" | sed "s#${rootdir}/##g")
   /usr/bin/php -q ${phpfile} > ${htmlfile}
-
-
   git add ${htmlfileinroot}
 done
+git commit -m 'Built samplefiles'
+git push origin master
