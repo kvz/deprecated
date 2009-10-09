4e4a0706ec03e7484cd5ed850ce70578e61e9d1b
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-09 15:02:46 +0200

    Built samplefiles

---
+++ php/samples/classes/KvzHTML/sample2-nonest-echo.php
@@ -0,0 +1,23 @@
+<?php
+if (!is_defined('DIR_KVZLIB')) {
+    define('DIR_KVZLIB', dirname(dirname(dirname(dirname(dirname(__FILE__))))));
+}
+
+// Sample starts here
+require_once DIR_KVZLIB.'/php/classes/KvzHTML.php';
+$E = new KvzHTML(array(
+    'echo' => true,
+));
+
+$E->p();
+    $E->span('You dont need to nest tags if you dont want to.');
+    $E->br(null);
+    $E->span('KvzHTML is flexible.');
+$E->p(false);
+
+$E->ul();
+    $E->li('Leaving a tag empty will just result in an open tag in HTML');
+    $E->li('Close tags with FALSE');
+    $E->li('For selfclosing tags like BR, use NULL');
+$E->ul(false);
+?>
