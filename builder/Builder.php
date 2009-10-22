0de019c2f886b44e8fa359d6b35e4a2f1fd503c2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-22 17:06:49 +0200

    Integrating eggshell

---
+++ builder/Builder.php
@@ -0,0 +1,358 @@
+<?php
+/**
+ * Description of Builder
+ *
+ * @author kvz
+ */
+class Builder extends EggShell{
+    protected $_config = array();
+
+    public function setConfig($config) {
+        $this->_config = $config;
+        $this->_users  = $this->_config['users'];
+        $this->_groups = $this->_config['groups'];
+        $this->_repos  = $this->_config['repos'];
+
+        $d = $this->_groups[$this->_config['devGroup']]['users'];
+        $this->_developers = array();
+        foreach($d as $i=>$username) {
+            $this->_developers[$username] = $this->_users[$username];
+        }
+    }
+
+    public function setPassword($username, $password) {
+        // Add developers as system users
+        if (!$this->userExists($username)) {
+            return $this->err('User %s does not exist yet. Cant change password now',
+                $username);
+        }
+        
+        // Change password
+        if (false === $this->userPasswd($username, $password, array(
+            'samba' => true,
+        )))  {
+            return false;
+        }
+        
+        $this->info('Changed password of user: %s', $username);
+        
+        return true;
+    }
+
+    public function createHomes() {
+        // Create Developer homes
+        foreach($this->_developers as $user) {
+            // Add developers as system users
+            if (!userExists($user['name'])) {
+                if (false === userAdd($user['name'], $this->_config['sysWebGroup'], null, null, $user['homedir'])) exit(1);
+                $this->info('Created user %s belonging to group: %s',
+                    $user['name'],
+                    $this->_config['sysWebGroup']);
+            }
+
+            // Go through repos
+            foreach($this->_repos as $reponame=>$repo) {
+                $this->mkdirOnce($this->projPath($repo, $user, 'etc'), 0755, true);
+                $this->mkdirOnce($this->projPath($repo, $user, 'Source'), 0755, true);
+                $this->mkdirOnce($this->projPath($repo, $user, 'log'), 0755, true);
+                $this->mkdirOnce($this->projPath($repo, $user, 'bin'), 0755, true);
+
+                // Vhosts
+                $file = $this->projPath($repo, $user, 'etc/default');
+                $res  = vhost($file, $user, $repo);
+                if (false === ($res)) {
+                    $this->err('Error while writing vhost: %s', $file);
+                    exit(1);
+                } elseif (null === $res) {
+                    $this->info('Skipped writing vhost: %s (force with --force)', $file);
+                } else {
+                    $this->info('Written %s', $file);
+                }
+
+                // Bin scripts
+                $script_pull = $this->projPath($repo, $user, 'bin/pull.sh');
+                $this->write($script_pull, 
+                    'cd '.$this->projPath($repo, $user, 'Source') . "\n" .
+                    'svn checkout --force '.sprintf($this->_config['svnUrl'], $repo['name']).'/'."\n"
+                );
+                $this->chmod($script_pull, 0744);
+
+                $script_reload = $this->projPath($repo, $user, 'bin/reload.sh');
+                $this->write($script_reload,
+                    'sudo /usr/sbin/apache2ctl configtest && sudo /usr/sbin/apache2ctl restart'. "\n"
+                );
+                $this->chmod($script_reload, 0744);
+
+                // Pull in Repos ?
+
+
+                // Chown Home
+                $this->chownr($user['homedir'], $user['name'], $this->_config['sysWebGroup']);
+            }
+        }
+
+        return true;
+    }
+
+    public function createApacheConf() {
+
+        // Save TCP information for Apache Listen instructions
+        $tcp[$repo['ip']][80] = true;
+        $tcp[$repo['ip']][$user['port']] = true;
+
+        foreach($this->_developers as $username=>$user) {
+            foreach($this->_repos as $reponame=>$repo) {
+                $this->appendOnce('/etc/apache2/httpd.conf', "Listen ".$repo['ip'].":".$user['port']."\n");
+            }
+        }
+
+        // Include all found developer vhosts to apache
+        // (even custom created ones)
+        $vpaths = $this->_config['basehome'] .'/*/Projects/*/etc/' ;
+        foreach (glob($vpaths) as $file) {
+            $this->appendOnce('/etc/apache2/httpd.conf', "Include ".$f."\n");
+        }
+
+        return true;
+    }
+
+    public function createIndexHtml() {
+        $indexH = htmIndex();
+        $this->write('/var/www/index.html', $indexH);
+        $this->info('Written %s', '/var/www/index.html');
+        return true;
+    }
+
+    public function htmIndex() {
+        // Index HTML
+        $html  = '<html>';
+        $html .= '<style>';
+        $html .= 'h1, .i1{padding-left: 0px;}';
+        $html .= 'h2, .i2{padding-left: 20px;}';
+        $html .= 'h3, .i3{padding-left: 40px;}';
+        $html .= 'h4, .i4{padding-left: 60px;}';
+        $html .= '</style>';
+        $html .= '<h1>Links</h1>';
+        $html .= '<ul>';
+            $html .= '<li>';
+                $html .= '<a href="https://'.$config['devDomain'].'/submin/login">https://'.$config['devDomain'].'/submin/login</a> (admin can manage repositories &amp; users)';
+            $html .= '</li>';
+            $html .= '<li>';
+                $html .= '<a href="https://www.truecare.nl">https://www.truecare.nl</a> (manage DNS records, create tickets, etc)';
+            $html .= '</li>';
+        $html .= '</ul>';
+        $html .= '<h1>Project Index</h1>';
+        $html .= '<p>These are the IPs & ports so it will Always work.
+        But you are free to attach any domain name or rewrite to these
+        addresses..</p>';
+
+        foreach($this->_repos as $reponame=>$repo) {
+            $html .= '<h2>'.$reponame.'</h2>';
+            $html .= $this->htmUserBlock($this->_developers['admin'], $repo);
+            foreach($this->_developers as $username=>$user) {
+                if ($username === 'admin') {
+                    continue;
+                }
+                $html .= $this->htmUserBlock($user, $repo);
+            }
+        }
+        
+        return $html;
+    }
+
+    public function htmUserBlock($user, $repo) {
+        $indent = 4;
+
+        $html  = '';
+        $urls = array(
+            'svn' => sprintf($this->_config['svnUrl'], $repo['name']),
+            'web' => sprintf($this->_config['webUrl'], $repo['ip'], $user['port']),
+            '-',
+            'ftp' => sprintf('ftp://%s@%s', $user['name'], $repo['ip']),
+            'sftp' => sprintf('ssh://%s@%s', $user['name'], $repo['ip']),
+            '-',
+            'Source' => $this->projPath($repo, $user, 'Source'),
+            'www' => 'Create a symlink from any directory in Source to '.$this->projPath($repo, $user, 'www'),
+            'log' => $this->projPath($repo, $user, 'log'),
+            'etc' => $this->projPath($repo, $user, 'etc'),
+            'bin' => $this->projPath($repo, $user, 'bin'),
+        );
+
+        if ($user['name'] === 'admin') {
+            $indent = 3;
+        } else {
+            $html .= '<h3>';
+            $html .= $user['name'];
+            $html .= '</h3>';
+        }
+
+        $html .= '<pre class="i'.$indent.'">';
+            foreach  ($urls as $key=>$url) {
+                if ($url === '-') {
+                    $html .= ''."\n";
+                    continue;
+                }
+                $html .= ''.str_pad($key.':', 7, ' ', STR_PAD_RIGHT).' ';
+                if ($key === 'web') {
+                    $html .= '<a href="'.$url.'">'.$url.'</a>';
+                } else {
+                    $html .= $url;
+                }
+                $html .= "\n";
+            }
+        $html .= '</pre>';
+
+        return $html;
+    }
+
+    /**
+     * Readz authz file and returns users, groups, repos
+     *
+     * @param <type> $config
+     * @return <type>
+     */
+    public function indexConfig($config = array()) {
+        if (is_string($config)) {
+            $config = require($config);
+        }
+
+        $config['repos'] = array();
+        $authSections    = parse_ini_file($config['authzFile'], true);
+        $userSections    = parse_ini_file($config['userFile'], true);
+        $repoCnt         = 0;
+        foreach ($authSections as $authSection=>$authKeys) {
+            if (substr($authSection, -2) === ':/') {
+                $repo = substr($authSection, 0, -2);
+                // repos
+                $config['repos'][$repo] = array (
+                    'name' => $repo,
+                    'repo' => $authKeys,
+                    'ip' => $this->claimIPPort($config, 'repo', $repo),
+                );
+            } elseif($authSection === 'groups') {
+                // groups
+                $config['groups'] = $authKeys;
+                $config['users']  = array();
+                foreach ($config['groups'] as $group => $userlist) {
+                    $groupusers = explode(', ', $userlist);
+                    $config['groups'][$group] = array(
+                        'users' => $groupusers,
+                        'name' => $group,
+                    );
+                    asort($config['groups'][$group]['users']);
+                    foreach($groupusers as $groupuser) {
+                        $config['users'][$groupuser]['groups'][] = $group;
+                    }
+                }
+
+                // users
+                foreach($config['users'] as $username => &$user) {
+                    $user['name'] = $username;
+                    $user['homedir'] = $config['basehome'].'/'.$username;
+                    $user['fullname'] = $userSections[$username]['fullname'];
+                    $user['email'] = $userSections[$username]['email'];
+                    $user['port'] = $this->claimIPPort($config, 'user', $username);
+                }
+                ksort($config['users']);
+                ksort($config['groups']);
+
+            } else {
+                // unknown
+            }
+        }
+
+        return $config;
+    }
+
+    public function claimIPPort($config, $type, $name) {
+        $ipArr = (array)@json_decode(@$this->write($config['ipFile']), true);
+
+        if ($type === 'user' && $name === 'admin') {
+            return 80;
+        }
+
+        if (!empty($ipArr[$type][$name])) {
+            return $ipArr[$type][$name];
+        }
+        if (empty($ipArr[$type])) {
+            $ipArr[$type] = array();
+        }
+
+        $cnt = count($ipArr[$type]) + 1;
+
+        if ($type === 'user') {
+            $val = $config['baseport'] + $cnt;
+        } elseif ($type === 'repo') {
+            $val = long2ip(ip2long($config['baseip']) + $cnt);
+        } else {
+            return false;
+        }
+
+        $ipArr[$type][$name] = $val;
+
+        return $this->write($config['ipFile'], json_encode($ipArr));
+    }
+
+    public function projPath($repo, $user, $type) {
+        if ($type === 'w') {
+            return 'w';
+        }
+
+        $path = $user['homedir'].'/Projects/'.$repo['name'].'/'.$type;
+        return $path;
+    }
+
+    public function createVHost($file, $user, $repo) {
+        global $config;
+
+        if (file_exists($file) && empty($config['force'])) {
+            return null;
+        }
+
+        $docroot = $this->projPath($repo, $user, 'www');
+        $source  = $this->projPath($repo, $user, 'Source');
+        $access  = $this->projPath($repo, $user, 'log/access.log');
+        $error   = $this->projPath($repo, $user, 'log/error.log');
+
+        if (!($template = $this->write($config['vhostTemplate']))) {
+            return false;
+        }
+
+        $vars = array(
+            '${email}' => $user['email'],
+            '${docroot}' => $docroot,
+            '${home}' => $user['homedir'],
+            '${domain}' => $repo['name'].'.'.$config['maindomain'],
+            '${ip}' => $repo['ip'],
+            '${port}' => $user['port'],
+            '${source}' => $source,
+            '${repo}' => $repo['name'],
+            '${user}' => $user['name'],
+            '${access_log}' => $access,
+            '${error_log}' => $error,
+        );
+
+        $template = str_replace(array_keys($vars), array_values($vars), $template);
+
+        if (!$this->write($file, $template)) {
+            return false;
+        }
+
+        $this->symlinkOnce($source, $docroot);
+
+        // touch log
+        touch($access);
+        $this->chgrp($access, $config['sysWebGroup']);
+        $this->chmod($access, 0664);
+
+        touch($error);
+        $this->chgrp($error, $config['sysWebGroup']);
+        $this->chmod($error, 0664);
+
+        return true;
+    }
+
+
+
+}
+?>
--- builder/build.php
+++ builder/build.php
@@ -2,147 +2,26 @@
 <?php
 require_once dirname(__FILE__).'/init.inc.php';
 
-// Parse CLI options
-$options = getArgs($argv);
-#info(print_r($options, true));
-// Add to config
-$config = array_merge($config, $options);
-#info(print_r($config, true));
+$Builder = new Builder(array(
+    'log-file' => '/var/log/builder.log',
+));
 
-extract($config);
+if (!isset($skipConf)) {
+    $config = $Builder->indexConfig('/etc/submin/builder/config.php');
+}
 
-// Password Hook
+$Builder->setConfig(array_merge($config, $Builder->getArgs($argv)));
+
 if (!empty($config['setpassword'])) {
     list($username, $password) = explode(':', $config['setpassword']);
-
-    // Add developers as system users
-    if (!userExists($username)) {
-        err('User %s does not exist yet and cannot be created in this stage. Try again later',
-            $username);
-    } else {
-        // Change password
-        $ret = exe('echo "%s:%s" | /usr/sbin/chpasswd',
-            escapeshellcmd($username),
-            escapeshellcmd($password));
-
-        // Bail out on fail
-        if (false === ($ret)) exit(1);
-
-        info('Changed password of user: %s', $username);
+    if (!$Builder->setPassword($username, $password)) {
+        exit(1);
     }
 }
 
-$index = array();
-$tcp = array();
+$Builder->createHomes();
 
-// Create Developer homes
-foreach($groups[$devGroup]['users'] as $i=>$username) {
-    $user = &$users[$username];
+$Builder->createApacheConf();
 
-    // Add developers as system users
-    if (!userExists($user['name'])) {
-        if (false === userAdd($user['name'], $config['sysWebGroup'], null, null, $user['homedir'])) exit(1);
-        info('Successfully created user %s belonging to group: %s',
-            $user['name'],
-            $config['sysWebGroup']);
-    }
-
-    // Go through repos
-    foreach($repos as $reponame=>$repo) {
-        @mkdir(projPath($repo, $user, 'etc'), 0755, true);
-        @mkdir(projPath($repo, $user, 'Source'), 0755, true);
-        @mkdir(projPath($repo, $user, 'log'), 0755, true);
-        @mkdir(projPath($repo, $user, 'bin'), 0755, true);
-
-        // Vhosts
-        $file = projPath($repo, $user, 'etc/default');
-        if (false === ($res = vhost($file, $user, $repo))) {
-            err('Error while writing vhost: %s', $file);
-            exit(1);
-        } elseif (null === $res) {
-            info('Skipped writing vhost: %s (force with --force)', $file);
-        } else {
-            info('Successfully written %s', $file);
-        }
-
-        // Bin scripts
-        $script_pull = projPath($repo, $user, 'bin/pull.sh');
-        file_put_contents($script_pull,
-            'cd '.projPath($repo, $user, 'Source') . "\n" .
-            'svn checkout --force '.sprintf($config['svnUrl'], $repo['name']).'/'."\n"
-        );
-        chmod($script_pull, 0744);
-        
-        $script_reload = projPath($repo, $user, 'bin/reload.sh');
-        file_put_contents($script_reload,
-            'sudo /usr/sbin/apache2ctl configtest && sudo /usr/sbin/apache2ctl restart'. "\n"
-        );
-        chmod($script_reload, 0744);
-
-        // Pull in Repos ?
-        
-
-        // Chown Home
-        chownr($user['homedir'], $user['name'], $config['sysWebGroup']);
-
-        // Save TCP information for Apache Listen instructions
-        $tcp[$repo['ip']][80] = true;
-        $tcp[$repo['ip']][$user['port']] = true;
-    }
-}
-
-// http.conf
-file_put_contents('/etc/apache2/httpd.conf', '');
-
-// Open ports
-foreach($tcp as $ip=>$p) {
-    foreach($p as $port=>$bool) {
-        file_put_contents('/etc/apache2/httpd.conf', "Listen ".$ip.":".$port."\n", FILE_APPEND);
-    }
-}
-
-// Include all found developer vhosts to apache
-// (even custom created ones)
-$p = $config['basehome'] .'/*/Projects/*/etc/' ;
-$includes = array();
-foreach (glob($p) as $f) {
-    $includes[] = "Include ".$f;
-}
-file_put_contents('/etc/apache2/httpd.conf', join("\n", $includes)."\n", FILE_APPEND);
-
-// Index HTML
-$indexH  = '<html>';
-$indexH .= '<style>';
-$indexH .= 'h1, .i1{padding-left: 0px;}';
-$indexH .= 'h2, .i2{padding-left: 20px;}';
-$indexH .= 'h3, .i3{padding-left: 40px;}';
-$indexH .= 'h4, .i4{padding-left: 60px;}';
-$indexH .= '</style>';
-$indexH .= '<h1>Links</h1>';
-$indexH .= '<ul>';
-    $indexH .= '<li>';
-        $indexH .= '<a href="https://'.$config['devDomain'].'/submin/login">https://'.$config['devDomain'].'/submin/login</a> (admin can manage repositories &amp; users)';
-    $indexH .= '</li>';
-    $indexH .= '<li>';
-        $indexH .= '<a href="https://www.truecare.nl">https://www.truecare.nl</a> (manage DNS records, create tickets, etc)';
-    $indexH .= '</li>';
-$indexH .= '</ul>';
-$indexH .= '<h1>Project Index</h1>';
-$indexH .= '<p>These are the IPs & ports so it will Always work.
-But you are free to attach any domain name or rewrite to these
-addresses..</p>';
-
-foreach($repos as $reponame=>$repo) {
-    $indexH .= '<h2>'.$reponame.'</h2>';
-    $indexH .= htmUserBlock($users['admin'], $repo);
-    foreach($groups[$devGroup]['users'] as $i=>$username) {
-        if ($username === 'admin') {
-            continue;
-        }
-        $indexH .= htmUserBlock($users[$username], $repo);
-    }
-}
-
-file_put_contents('/var/www/index.html', $indexH);
-info('Written %s', '/var/www/index.html');
+$Builder->createIndexHtml();
 ?>
