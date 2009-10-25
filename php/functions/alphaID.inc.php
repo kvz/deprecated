323e9c3bb3e489150bdddea51a785e1e931003d7
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-25 13:11:26 +0100

    Password patch by Simon Franz

--- php/functions/alphaID.inc.php
+++ php/functions/alphaID.inc.php
@@ -71,20 +71,46 @@
  * </code>
  * 
  * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @author    Simon Franz
  * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
  * @version   SVN: Release: $Id: alphaID.inc.php 344 2009-06-10 17:43:59Z kevin $
  * @link      http://kevin.vanzonneveld.net/
  * 
- * @param mixed   $in     String or long input to translate     
- * @param boolean $to_num Reverses translation when true
- * @param mixed   $pad_up Number or boolean padds the result up to a specified length
+ * @param mixed   $in      String or long input to translate
+ * @param boolean $to_num  Reverses translation when true
+ * @param mixed   $pad_up  Number or boolean padds the result up to a specified length
+ * @param string  $passKey Supplying a password makes it harder to calculate the original ID
  * 
  * @return mixed string or long
  */
-function alphaID($in, $to_num = false, $pad_up = false)
+function alphaID($in, $to_num = false, $pad_up = false, $passKey = null)
 {
     $index = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if ($passKey !== null) {
+        // Although this function's purpose is to just make the
+        // ID short - and not so much secure,
+        // with this patch by Simon Franz (http://blog.snaky.org/)
+        // you can optionally supply a password to make it harder
+        // to calculate the corresponding numeric ID
+        
+        for ($n = 0; $n<strlen($index); $n++) {
+            $i[] = substr( $index,$n ,1);
+        }
+
+        $passhash = hash('sha256',$passKey);
+        $passhash = (strlen($passhash) < strlen($index))
+            ? hash('sha512',$passKey)
+            : $passhash;
+
+        for ($n=0; $n < strlen($index); $n++) {
+            $p[] =  substr($passhash, $n ,1);
+        }
+        
+        array_multisort($p,  SORT_DESC, $i);
+        $index = implode($i);
+    }
+
     $base  = strlen($index);
 
     if ($to_num) {
