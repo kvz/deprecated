7860b4239f0794f7c68614bb5bd16aea084acde8
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-06-17 11:37:40 +0200

    expose basic curl functions to outside world

--- RestClient.php
+++ RestClient.php
@@ -17,16 +17,16 @@ class RestClient {
     public $request_prefix = 'http://';
     public $request_suffix = '';
     public $error = '';
-    
+
     protected $url;
-    
+
     protected $Curl;
     protected $response_type;
     protected $response_types = array();
 
     protected $failCodes  = array(403);
     protected $crashCodes = array(404, 500);
-    
+
     public function __construct($request_prefix = false, $request_suffix = false, $restOpts = array()) {
         $this->Curl = new Curl();
         if (array_key_exists('userAgent', $restOpts)) {
@@ -38,13 +38,13 @@ class RestClient {
         if (array_key_exists('verifySSL', $restOpts)) {
             $this->Curl->verify_ssl = $restOpts['verifySSL'];
         }
-        
+
         $this->add_response_type('json', array('RestClientResponse', 'json'), '.json');
         $this->add_response_type('text', array('RestClientResponse', 'text'), '');
         $this->add_response_type('xml', array('RestClientResponse', 'xml'), '.xml');
-        
+
         $this->set_response_type('text');
-        
+
         if ($request_prefix) $this->request_prefix = $request_prefix;
         if ($request_suffix) $this->request_suffix = $request_suffix;
     }
@@ -66,19 +66,19 @@ class RestClient {
     public function delete($url, $vars = array()) {
         return $this->request('delete', $url, $vars);
     }
-    
+
     public function get($url, $vars = array()) {
         return $this->request('get', $url, $vars);
     }
-    
+
     public function post($url, $vars = array()) {
         return $this->request('post', $url, $vars);
     }
-    
+
     public function put($url, $vars = array()) {
         return $this->request('put', $url, $vars);
     }
-    
+
     public function add_response_type($type, $callback, $request_suffix = '') {
         if (is_array($callback)) {
             $object = $callback[0];
@@ -95,7 +95,7 @@ class RestClient {
             return false;
         }
     }
-    
+
     public function set_response_type($type, $set_request_suffix = true) {
         $type = strtolower($type);
         if (in_array($type, array_keys($this->response_types))) {
@@ -111,19 +111,26 @@ class RestClient {
     public function lastUrl() {
         return $this->url;
     }
-    
+
+    public function curlGet($url, $vars) {
+         return $this->Curl->get($url, $vars);
+    }
+    public function curlPost($url, $vars) {
+         return $this->Curl->post($url, $vars);
+    }
+
     protected function request($method, $url, $vars = array()) {
         if ($method != 'get') $vars['_method'] = $method;
         $this->url = $this->request_prefix.$url.$this->request_suffix;
-        $response  = ($method == 'get') ? $this->Curl->get($this->url, $vars) : $this->Curl->post($this->url, $vars);
+        $response  = ($method === 'get') ? $this->curlGet($this->url, $vars) : $this->curlPost($this->url, $vars);
         $crash     = $fail = false;
-        
+
         if ($response) {
             if (($fail = in_array($response->headers['Status-Code'], $this->failCodes))
                 || ($crash = in_array($response->headers['Status-Code'], $this->crashCodes))) {
                 $this->error = 'Request to "'.$this->url.'" responded with a ' . $response->headers['Status'];
             }
-            
+
             if ($crash) {
                 return false;
             }
@@ -141,4 +148,3 @@ class RestClient {
         }
     }
 }
-?>
