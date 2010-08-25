9a47b39ccd7383e7e5ecf982d2e609bc98b3adf3
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-08-25 20:45:56 +0200

    Curl uploading of source working!!

--- app/controllers/orms_controller.rb
+++ app/controllers/orms_controller.rb
@@ -1,4 +1,6 @@
 class OrmsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   # GET /posts
   # GET /posts.xml
   def index
@@ -10,7 +12,7 @@ class OrmsController < ApplicationController
     end
   end
 
-# GET /orms/1
+  # GET /orms/1
   # GET /orms/1.xml
   def show
     @orm = Orm.find(params[:id])
@@ -18,8 +20,6 @@ class OrmsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @orm }
-      format.svg  { render :svg => @orm }
-      # format.png  { render :png => @orm }
     end
   end
 
@@ -42,16 +42,28 @@ class OrmsController < ApplicationController
   # POST /orms
   # POST /orms.xml
   def create
-    @orm = Orm.new(params[:orm])
+    # Convert the multipart source to plain text source field
+    # for use with cat server.php | curl -F orm[source]=@- http://ormify.com/orms/
+    if File.file?(params[:orm][:source])
+      data = ''
+      params[:orm][:source].each_line do |line|
+        data += line
+      end
+      params[:orm][:source] = data
+      curled = true
+    end
 
+    @orm = Orm.new(params[:orm])
     respond_to do |format|
       if @orm.save
         flash[:notice] = 'Orm was successfully created.'
         format.html { redirect_to(@orm) }
         format.xml  { render :xml => @orm, :status => :created, :location => @orm }
+        format.text 
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @orm.errors, :status => :unprocessable_entity }
+        format.text { render :text => @orm.errors, :status => :unprocessable_entity }
       end
     end
   end
--- app/models/orm.rb
+++ app/models/orm.rb
@@ -1,5 +1,6 @@
 class Orm < ActiveRecord::Base
   set_primary_key "uuid"
   belongs_to :type
+  validates_presence_of :source
   include UUIDHelper
 end
