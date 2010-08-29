8bb9273eb13e8498c94c1649394b7a28e18d8b8d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-08-29 20:06:16 +0200

    Move payment to orms controller. Skip confirm step

--- app/controllers/orms_controller.rb
+++ app/controllers/orms_controller.rb
@@ -1,6 +1,50 @@
 class OrmsController < ApplicationController
+  include ActiveMerchant::Billing
   skip_before_filter :verify_authenticity_token
-  
+  attr_accessor :price
+  @@price = 50
+
+  def checkout
+    setup_response = gateway.setup_purchase(@@price,
+      :ip                => remote_ip,
+      :return_url        => url_for(:action => 'complete', :id => params[:id], :only_path => false),
+      :cancel_return_url => url_for(:action => 'show', :id => params[:id], :only_path => false)
+    )
+    redirect_to gateway.redirect_url_for(setup_response.token)
+  end
+
+  def confirm
+    redirect_to :action => 'show', :id => params[:id] unless params[:token]
+
+    details_response = gateway.details_for(params[:token])
+
+    if !details_response.success?
+      @message = details_response.message
+      render :action => 'error'
+      return
+    end
+    @address = details_response.address
+  end
+
+  def complete
+    purchase = gateway.purchase(@@price,
+      :ip       => request.remote_ip,
+      :token    => params[:token],
+      :payer_id => params[:PayerID]
+      #:payer_id => params[:payer_id],
+    )
+
+    if !purchase.success?
+      @message = purchase.message
+      render :action => 'error'
+      return
+    else
+      flash[:notice] = 'You\'ve payed for this ORM'
+      redirect_to :action => 'show', :id => params[:id]
+    end
+  end
+
+
   # GET /posts
   # GET /posts.xml
   def index
@@ -84,5 +128,14 @@ class OrmsController < ApplicationController
         format.xml  { render :xml => @orm.errors, :status => :unprocessable_entity }
       end
     end
+  end
+  
+  private
+  def gateway
+    @gateway ||= PaypalExpressGateway.new(
+      :login => 'kvz_1283090817_biz_api1.php.net',
+      :password => '1283090824',
+      :signature => 'A9umzV03tO-84Sc8dF5JkGDvC67MAlbshaqX36YkXBvIaWcGG9LVIri5'
+    )
   end
 end
