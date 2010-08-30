817f51f8a674f1bb70630db4abc422c71c3b92c7
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-08-30 03:41:41 +0200

    Basic graphing working! Not for phpjs models tho.. 3 models and multiple references to author itself cant be right

--- app/models/orm.rb
+++ app/models/orm.rb
@@ -19,14 +19,11 @@ class Orm < ActiveRecord::Base
     typePatterns = {
       :cakephp => {
         :defin => /^\s*([Cc]lass\s+([A-Z][A-Za-z0-9_]+)\s+[Ee][Xx][Tt][Ee][Nn][Dd][Ss]\s+[A-Z][A-Za-z0-9_]+Model)\s*\{(.+)\}/sm,
-        :child => /(belongsTo)\s+=\s+(array\s*\([^;]+)/,
-        :paren => /(hasMany|hasOne|hasAndBelongsToMany)\s+=\s+(array\s*\(>[^;]+)/,
+        :child => /^\s*(public|var)\s+\$(belongsTo)\s+=\s+(array\s*\([^;]+)/sm,
+        :paren => /^\s*(public|var)\s+\$(hasMany|hasOne|hasAndBelongsToMany)\s+=\s+(array\s*\([^;]+)/sm,
       }
     }
     
-    parsed = {}
-    connections = {}
-    connected = {}
 
     # Split source into models per type
     typeModels = {}
@@ -40,61 +37,109 @@ class Orm < ActiveRecord::Base
     typeModels.sort_by {|models| models.size}
 
     # Process each model's hasbtm properties
+    parsed = {}
     typeModels.each do |type, models|
       models.each do |modelcol|
         modelDef, modelName, modelSource = modelcol
         parsed[modelName] = {:child => {}, :paren => {}}
         # child to
         if source = modelSource.match(typePatterns[type][:child])
-          parsed[modelName][:child] = array_to_ruby type, source[2]
+          parsed[modelName][:child] = array_to_ruby type, source[3]
         end
         # parent of
         if source = modelSource.match(typePatterns[type][:paren])
-          parsed[modelName][:paren] = array_to_ruby type, source[2]
+          parsed[modelName][:paren] = array_to_ruby type, source[3]
         end
       end
     end
     
     # Make connections
+    # which I knew how to make this compact without raising errors.
+    connections = {}
+    connected = {}
     parsed.each do |modelName, modelRelations|
-      connections = connected = {:modelName => {}}
       modelRelations.each do |relationType, relations|
         relations.each do |relationName, relationData|
-          
+          relationName = relationData[:relationName]
+
+          if relationType == :child
+            if !connections.has_key? modelName
+              connections[modelName] = {}
+            end
+            if !connections[modelName].has_key? relationName
+              connections[modelName][relationName] = {}
+            end
+            connections[modelName][relationName][relationType] = relationName
+          else
+            if !connections.has_key? relationName
+              connections[relationName] = {}
+            end
+            if !connections[relationName].has_key? modelName
+              connections[relationName][modelName] = {}
+            end
+            connections[relationName][modelName][relationType] = relationName
+          end
+
+          if !connected.has_key? modelName
+            connected[modelName] = 0
+          end
+          connected[modelName] += 1
+          if !connected.has_key? relationName
+            connected[relationName] = 0
+          end
+          connected[relationName] += 1
         end
       end
     end
 
-
-   return parsed.inspect
-
+    models = parsed
+    return [models, connected, connections]
   end
 
   def array_to_ruby(type, str)
     case type
     when :cakephp
+      # Let PHP Save the array code as json
       phpCode = "<?php echo json_encode(" + str + ");"
       phpFile = '/tmp/' + self.uuid;
       tmpFile = File.new(phpFile, 'w')
       tmpFile.write(phpCode)
       tmpFile.close
-      
+     
+      # Let Ruby parse the json so we have an actual enumerable
       require 'open3'
       require 'json'
       cmd = "php " + phpFile
       stdin, stdout, stderr = Open3.popen3(cmd)
-      
       obj = JSON.parse(stdout.readlines.join)
-      return obj
-      obj.each do|name, relationData|
-        if !relationData.responds_to?(each)
-          
+      
+      # Consistently format relationData
+      # and add relationName & className 
+      @prop = {}
+      obj.each do|name, data|
+        # Nest if string
+        if data.kind_of?(String)
+          name = data
         end
-        if !relationData[:relationName]
-          relationData[:relationName] = name
+        if !data.kind_of?(Hash)
+          data = {}
+        end
+        if !name.kind_of?(String)
+          raise str + ' results in non-string name: ' + name.inspect
+        end
+
+        @prop[name] = data
+        if !@prop.has_key? :relationName
+          @prop[name][:relationName] = name
+        end
+        if !@prop.has_key? :className
+          @prop[name][:className] = name
         end
       end
+
+      return @prop
     end
+    raise "Unsupported type: " + type
   end
 
   def build_graph()
@@ -105,36 +150,69 @@ class Orm < ActiveRecord::Base
     # http://www.omninerd.com/articles/Automating_Data_Visualization_with_Ruby_and_Graphviz
     
     if !self.payed
-      ext = 'png'
+      exts = ['png']
       watermark = 'Preview. Buy full version at http://ormify.com/' + self.uuid
     else
-      ext = 'svg' 
+      exts = ['svg'] 
+    end
+  
+    edgeOpts = {:arrowsize => 0.6, :weight => 1, :fontsize => 11, :fontname => 'arial', :fontcolor => 'gray45', :color => 'gray77'}
+    graphOpts = {:normalize => true, :ratio => 0.3, :ranksep => 0.9, :nodesep => 0.1, :rankdir => 'BT'}
+    nodeOpts = {
+      :fontsize => 12,
+      :fontname => 'arial',
+      :color => '1 1 1',
+      :shape => 'square',
+      :height => 0.8
+    }
+    
+    # Initialize Data
+    models, connected, connections = self.parse_source
+    url = '/graphs/' + self.uuid + '.' + 'EXT'
+    file = File.join(RAILS_ROOT, 'public') + url
+    
+    
+    # Create a new graph
+    require 'graphviz'
+    g = GraphViz.new :G, graphOpts
+
+    # Add nodes
+    connected.each do|modelName, count|
+      g.add_node modelName, nodeOpts.merge(:label => modelName)
     end
 
-    url = '/graphs/' + self.uuid + '.' + ext
-    file = File.join(RAILS_ROOT, 'public') + url
+    # Add edges
+    connections.each do|from, tos|
+      tos.each do|to, types|
+        dirs = {:back => false, :forward => false}
+        edgeOpts[:label] = ''
 
-    require 'graphviz'
+        if types.count == 2 then
+          edgeOpts[:dir] = 'both'
+        end
 
-    # Create a new graph
-    parsed = self.parse_source
-    puts parsed
-    
-    g = GraphViz.new( :G, :type => :digraph )
-
-    # Create two nodes
-    hello = g.add_node( "Hello" )
-    world = g.add_node( "World" )
+        types.each do|type, name|
+          if type == :child && to != name
+              edgeOpts[:label] = name
+          end
+          if type == :paren && from != name
+              edgeOpts[:label] = name
+          end
+        end
+        g.add_edge from, to, edgeOpts
+      end
+    end
 
     # Create an edge between the two nodes
-    g.add_edge( hello, world )
+    #g.add_edge( hello, world )
 
-    # Generate output image
-    #g.output( :png => "hello_world.png" )
-    g.output( "output" => ext, :file => file)
+    # Generate output images
+    exts.each do |ext|
+      g.output( "output" => ext, :file => file.gsub('EXT', ext))
+    end
     
-    self.file = file
-    self.url = url
+    self.file = file.gsub('EXT', exts.first)
+    self.url = url.gsub('EXT', exts.first)
     self.save!
   end
 end
